8b5f3cabad8cc605f4ba4b2c3da71f45bdd7647c
Raychen0617 <raychen11532.cs07@nctu.edu.tw>
2022-08-17 14:45:34 +0800

    update_match_for_better_inputs

--- optimizer/match.py
+++ optimizer/match.py
@@ -23,31 +23,55 @@ Match two pytorch models with same structure, but different hyperparameters (ex:
 
 '''
 
-
-def match_nas(ori_model='./models/yolov5s.yaml', nas_model="./models/yolov5sb_nas.yaml", nas_json="./output/nas_yolov5sb.json", save="./checkpoint/nas_yolov5s.pt"):
+def extract_backbone():
     
     device = torch.device("cpu")
-    yolo = Model(ori_model).to(device=device)    
-    nas = NASBACKBONE(cfg=nas_model, nc=200).to(device=device).backbone.model
-    nasjson = json.load(open(nas_json))
     dummy_input = torch.rand(1, 3, 640, 640)
-    
+
+    if isnas:
+
+        model = NASBACKBONE(cfg=nas_model, nc=200).to(device=device).backbone.model
+        nasjson = json.load(open(nas_json))
+        # fixed nas architecture 
+        for i in range(len(nas)):
+            if isinstance(nas[i], NASConv):
+                nas[i].conv = nas[i].conv[nasjson["nasconv_{}".format(i)]]
+            elif isinstance(nas[i], NASC3) :
+                nas[i].cv1.conv = nas[i].cv1.conv[nasjson["nasconv_{}-1".format(i)]]
+                nas[i].cv2.conv = nas[i].cv2.conv[nasjson["nasconv_{}-2".format(i)]]
+
+    else:
+        model = torch.load(model_backbone_checkpoint).to(device=device).backbone
+
+
+def fixed_nas(nas_backbone, nas_json):
+
+    # This function fixed nas backbone with given json file 
+
+    nasjson = json.load(open(nas_json))
+
     # fixed nas architecture 
-    for i in range(len(nas)):
-        if isinstance(nas[i], NASConv):
-            nas[i].conv = nas[i].conv[nasjson["nasconv_{}".format(i)]]
-        elif isinstance(nas[i], NASC3) :
-            nas[i].cv1.conv = nas[i].cv1.conv[nasjson["nasconv_{}-1".format(i)]]
-            nas[i].cv2.conv = nas[i].cv2.conv[nasjson["nasconv_{}-2".format(i)]]
-    
-    #exit()
-    
+    for i in range(len(nas_backbone)):
+        if isinstance(nas_backbone[i], NASConv):
+            nas_backbone[i].conv = nas_backbone[i].conv[nasjson["nasconv_{}".format(i)]]
+        elif isinstance(nas_backbone[i], NASC3) :
+            nas_backbone[i].cv1.conv = nas_backbone[i].cv1.conv[nasjson["nasconv_{}-1".format(i)]]
+            nas_backbone[i].cv2.conv = nas_backbone[i].cv2.conv[nasjson["nasconv_{}-2".format(i)]]
+    return nas_backbone
+
+
+# This function construct yolov5 - nas by providing  .json (choice) and .yaml 
+def match_nas(yolo, nas_backbone, nas_json, save):
+
+    device = torch.device("cpu")
+    nas = fixed_nas(nas_backbone, nas_json)
+    dummy_input = torch.rand(1, 3, 640, 640)
+
     # store nas layers
     nas_layer = {}
     for name, model_type in nas.named_modules():
         if isinstance(model_type, Conv) or isinstance(model_type, NASConv) or isinstance(model_type, NASC3):
             nas_layer[name] = model_type
-
 
     for name, model_type in yolo.model.named_modules():
         
@@ -60,10 +84,6 @@ def match_nas(ori_model='./models/yolov5s.yaml', nas_model="./models/yolov5sb_na
                 model_type.conv = nas_layer[name].conv
                 model_type.bn = nas_layer[name].bn
     
-    #print(yolo)
-    #return
-    #exit()
-
     # retune model size for cspnet concat 
     yolo.model[9].cv1.conv = nn.Conv2d(yolo.model[8].cv3.conv.out_channels, yolo.model[9].cv1.conv.out_channels, kernel_size=(1, 1), stride=(1, 1), bias=False)
     yolo.model[10].conv = nn.Conv2d(yolo.model[9].cv2.conv.out_channels, yolo.model[10].conv.out_channels, kernel_size=(1, 1), stride=(1, 1), bias=False)
@@ -73,16 +93,14 @@ def match_nas(ori_model='./models/yolov5s.yaml', nas_model="./models/yolov5sb_na
     yolo.model[17].cv2.conv = nn.Conv2d(yolo.model[14].conv.out_channels + yolo.model[4].cv3.conv.out_channels, yolo.model[17].cv2.conv.out_channels, kernel_size=(1, 1), stride=(1, 1), bias=False)
     yolo.to(device=device)
 
-    #print(yolo)
-
     # load nas state dict back 
     yolo.model.load_state_dict(nas.state_dict(), strict=False)
     print("Matching two different models ", yolo(dummy_input)[0].shape == (1, 3, 80, 80, 85))
-
     torch.save(yolo, save)
     print("Save at ", save)
 
 
+# This function matches the pruned backbone back to yolov5 
 def match(ori_model='./models/yolov5s.yaml', pruned_model="./checkpoint/pruned_yolov5sb.pt", save="./checkpoint/pruned_yolov5s.pt"):
 
     device = torch.device("cpu")
