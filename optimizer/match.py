7f040824de070c97e0ad53b460696c1bca62c971
Raychen0617 <raychen11532.cs07@nctu.edu.tw>
2022-10-26 10:12:11 +0800

    add fix_nas_yolo(backbone)

--- optimizer/match.py
+++ optimizer/match.py
@@ -100,35 +100,66 @@ def conv_match(new, old):
     new.bn = old.bn
     return new
 
-def nas_fix_backbone_match(yolo, pruned_yolo):
+def fix_nasbackbone(backbone, backbone_nas):
     
-    # This function matches pruned yolo with original yolo 
+    # This function matches nas backbone to original backbone
 
     device = torch.device("cpu")
     dummy_input = torch.rand(1, 3, 640, 640)
 
-    for name, module in pruned_yolo.named_children():
-        pruned_yolo_layer = []
+    for name, module in backbone_nas.named_children():
+        backbone_nas_layer = []
         for submodule in module:
-            pruned_yolo_layer.append(submodule)
+            backbone_nas_layer.append(submodule)
+
+    for name, module in backbone.named_children():
+        
+        for layerid in range(len(backbone_nas_layer)):
+            #print(module[layerid], type(module[layerid]))
+            if isinstance(module[layerid], Conv):
+                module[layerid] = conv_match(module[layerid], backbone_nas_layer[layerid])
+            
+            elif isinstance(module[layerid], C3):
+                module[layerid].cv1 = conv_match(module[layerid].cv1, backbone_nas_layer[layerid].total.cv1)
+                module[layerid].cv2 = conv_match(module[layerid].cv2, backbone_nas_layer[layerid].total.cv2)
+                module[layerid].cv3 = conv_match(module[layerid].cv3, backbone_nas_layer[layerid].total.cv3)
+                module[layerid].m = backbone_nas_layer[layerid].total.m
+
+            else:
+                print("sth gets wrong !!!!, please checkout optimizer/match.py")
+                exit()
+    return backbone
+
+def fix_nasyolo(yolo, nas_yolo, backbone_layer=8):
+    
+    # This function matches nas yolo to original yolo 
+
+    device = torch.device("cpu")
+    dummy_input = torch.rand(1, 3, 640, 640)
+
+    for name, module in nas_yolo.named_children():
+        nas_yolo_layer = []
+        for submodule in module:
+            nas_yolo_layer.append(submodule)
 
     for name, module in yolo.named_children():
         
-        for layerid in range(len(pruned_yolo_layer)):
-            #print(module[layerid], type(module[layerid]))
+        for layerid in range(backbone_layer):
+            
             if isinstance(module[layerid], Conv):
-                module[layerid] = conv_match(module[layerid], pruned_yolo_layer[layerid])
+                module[layerid] = conv_match(module[layerid], nas_yolo_layer[layerid])
             
             elif isinstance(module[layerid], C3):
-                module[layerid].cv1 = conv_match(module[layerid].cv1, pruned_yolo_layer[layerid].total.cv1)
-                module[layerid].cv2 = conv_match(module[layerid].cv2, pruned_yolo_layer[layerid].total.cv2)
-                module[layerid].cv3 = conv_match(module[layerid].cv3, pruned_yolo_layer[layerid].total.cv3)
-                module[layerid].m = pruned_yolo_layer[layerid].total.m
+                module[layerid].cv1 = conv_match(module[layerid].cv1, nas_yolo_layer[layerid].total.cv1)
+                module[layerid].cv2 = conv_match(module[layerid].cv2, nas_yolo_layer[layerid].total.cv2)
+                module[layerid].cv3 = conv_match(module[layerid].cv3, nas_yolo_layer[layerid].total.cv3)
+                module[layerid].m = nas_yolo_layer[layerid].total.m
 
             else:
                 print("sth gets wrong !!!!, please checkout optimizer/match.py")
                 exit()
     return yolo
+
 
 def match(yolo, pruned_yolo, save):
     
@@ -143,8 +174,6 @@ def match(yolo, pruned_yolo, save):
         if isinstance(model_type, NASConv) or isinstance(model_type, NASC3) or isinstance(model_type, Conv):
             pruned_yolo_layer[name] = model_type
 
-    #print(pruned_yolo_layer.items()
-
     for name, model_type in yolo.named_modules():
         if isinstance(model_type, Conv):
             
@@ -152,7 +181,7 @@ def match(yolo, pruned_yolo, save):
                 print(name, "cannot be found")
 
             else:
-                print(name, pruned_yolo_layer[name], model_type.act)
+                #print(name, pruned_yolo_layer[name], model_type)
                 if pruned_yolo_layer[name].conv.in_channels != model_type.conv.in_channels  or pruned_yolo_layer[name].conv.out_channels != model_type.conv.out_channels \
                     or pruned_yolo_layer[name].conv.kernel_size != model_type.conv.kernel_size  or pruned_yolo_layer[name].conv.padding != model_type.conv.padding:
                     model_type.conv = pruned_yolo_layer[name].conv
@@ -162,9 +191,7 @@ def match(yolo, pruned_yolo, save):
                 
                 if pruned_yolo_layer[name].act != model_type.act:
                     model_type.act = pruned_yolo_layer[name].act
-                
-
-
+            
     # retune model size for cspnet concat 
     yolo.model[9].cv1.conv = nn.Conv2d(yolo.model[8].cv3.conv.out_channels, yolo.model[9].cv1.conv.out_channels, kernel_size=(1, 1), stride=(1, 1), bias=False)
     yolo.model[10].conv = nn.Conv2d(yolo.model[9].cv2.conv.out_channels, yolo.model[10].conv.out_channels, kernel_size=(1, 1), stride=(1, 1), bias=False)
