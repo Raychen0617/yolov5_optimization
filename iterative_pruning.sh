3f64fa88ce5676ee20ccf0f6886f1c16a1d2c1a2
Raychen0617 <raychen11532.cs07@nctu.edu.tw>
2022-11-29 15:42:09 +0800

    small_name_changed

--- iterative_pruning.sh
+++ iterative_pruning.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python iterative_pruning.py --yolo "./checkpoint/multi-trail_yolov5s.pt" --save_path "./iterative_pruning/yolo.pt"
+python iterative_pruning.py --yolo "./checkpoint/multi_nas_yolov5s.pt" --save_path "./iterative_pruning/yolo.pt"
 python train.py --data coco.yaml --weights "./iterative_pruning/yolo.pt" --img 640  --epochs 5
 
 new_weight=$(ls ./runs/train/ | tail -n 1)
